75efa58efeea6d68eb2133e96c1f76ebcb014d5f
Alessandro Aprile <a.aprile@kevopartners.com>
2021-10-08 17:06:07 +0200

    resset exercise to original

--- src/components/Inputs.js
+++ src/components/Inputs.js
@@ -1,42 +1,13 @@
-import {AiOutlinePlusCircle} from "react-icons/ai";
-import {useState} from "react";
+import { AiOutlinePlusCircle } from "react-icons/ai";
 
-/*
-export const Inputs = (props) => {
+export const Inputs = () => {
   return (
     <div className="inputs-container">
-        <form>
       <input type="text" placeholder="Add Todo" />
-
-        </form>
-      {/!*<button className="input-button" onClick={(event) => props.onGetTask(event)}>
+      <button className="input-button" onClick={() => console.log("ADD TODO")}>
         <AiOutlinePlusCircle />
         Add
-      </button>*!/}
+      </button>
     </div>
   );
-};*/
-
-
-
-export const Inputs = (props) => {
-    const [taskName, setTaskName] = useState('')
-
-    const onChangeHandler = (event) => {
-        console.log(event.target.value, "onChangeHandler()");
-        setTaskName(event.target.value);
-        console.log(taskName)
-    }
-
-    return (
-        <div className="inputs-container">
-            <form onSubmit={(event)=> props.onGetTask(event, taskName)}>
-                <input id="taskName" type="text" onChange={onChangeHandler} placeholder="Add Todo"/>
-                <button type="submit" className="input-button">
-                    <AiOutlinePlusCircle/>
-                    Add
-                </button>
-            </form>
-        </div>
-    );
 };
